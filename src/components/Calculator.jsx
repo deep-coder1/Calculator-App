e840526e2c0a7669938f317b255c7b42f7ba1cc8
Deepak Thakur <ndktbd@gmail.com>
2026-08-24 12:08:48 +0530

    div tag and css class add under div

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -2,11 +2,23 @@ import { useState } from "react";
 
 
 const Calculator = () => {
-
+  
 
   return (
     <div className="w-full max-w-[420px]">
-      
+      <div
+        className="
+          rounded-[32px]
+          bg-white
+          p-4
+          sm:p-6
+          shadow-2xl
+          border
+          border-gray-100
+        "
+      >
+        
+      </div>
     </div>
   );
 };
