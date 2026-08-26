1b6532795eee1be1360f2e9bf9f25db7415003cb
Deepak Thakur <ndktbd@gmail.com>
2026-08-26 19:31:18 +0530

    In header div under div and css class add

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -155,6 +155,10 @@ const Calculator = () => {
       >
         {/* Header */}
         <div className="flex items-center justify-between mb-5 px-1">
+          <div>
+            
+          </div>
+
           
         </div>
 
