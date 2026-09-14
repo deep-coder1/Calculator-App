dd8903fe289a26b83909c47a72d50b5db7a36e67
Deepak Thakur <ndktbd@gmail.com>
2026-09-14 18:56:10 +0530

    Button add and mui button import

--- src/components/CalculatorButton.jsx
+++ src/components/CalculatorButton.jsx
@@ -1,10 +1,17 @@
-
+import { Button } from "@mui/material";
 
 const CalculatorButton = ({
   
 }) => {
   return (
-    
+    <Button
+      variant={variant}
+      color={color}
+      onClick={onClick}
+      
+    >
+      Children
+    </Button>
   );
 };
 
