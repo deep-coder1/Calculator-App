5710bfe01295c1c6a915ee3d221491e0fb99b9da
Deepak Thakur <ndktbd@gmail.com>
2026-09-19 16:25:51 +0530

    Add css class in Calculator button comp

--- src/components/CalculatorButton.jsx
+++ src/components/CalculatorButton.jsx
@@ -12,7 +12,18 @@ const CalculatorButton = ({
       variant={variant}
       color={color}
       onClick={onClick}
-      
+      className={`
+        !min-h-[60px]
+        sm:!min-h-[65px]
+        !rounded-2xl
+        !text-lg
+        sm:!text-xl
+        !font-semibold
+        transition-all
+        duration-200
+        active:scale-95
+        ${className}
+      `}
     >
       {children}
     </Button>
